3687dba39d07748d8a2677ab1e40ed48700569ee
Alex deQuevedo <alexdequevedo@gmail.com>
2020-11-14 12:10:39 -0500

    save

--- scripts/mysql-actions.py
+++ scripts/mysql-actions.py
@@ -22,7 +22,7 @@ def create_yearly_stats():
     CREATE TABLE yearly_stats (
         owner VARCHAR(255),
         year INT,
-        team_name VARCHAR(255), 
+        team_name VARCHAR(255),
         wins INT,
         losses INT,
         points_for INT,
@@ -32,23 +32,23 @@ def create_yearly_stats():
         primary KEY (owner, year)
     )
     ''')
-    
+
 def insert_data(owner, year, team_name, wins, losses, points_for, points_against, season_finish, final_finish):
     sql = 'INSERT INTO yearly_stats (owner, year, team_name, wins, losses, points_for, points_against, season_finish, final_finish) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
     vals = (owner, year, team_name, wins, losses, points_for, points_against, season_finish, final_finish)
     mycursor = mydb.cursor()
     mycursor.execute(sql, vals)
     mydb.commit()
-    
+
 def yearly_historical_inserts():
     year_start=2014
     year_end=2020
     league_id='1721747'
     print_json=False
-    
+
     ESPN_S2=espn_s2
     SWID=swid
-    
+
     stats = {}
     reg="\((.*?)\)"
     tmp2 = {}
@@ -73,31 +73,31 @@ def yearly_historical_inserts():
                 'mov': team.mov,
                 'schedule': [re.search(reg, str(schd)).group() for schd in team.schedule]
             }
-            insert_data(team.owner, year, team.team_name, team.wins, team.losses, team.points_for, team.points_against, team.standing, team.final_standing)
+            #insert_data(team.owner, year, team.team_name, team.wins, team.losses, team.points_for, team.points_against, team.standing, team.final_standing)
             if not tmp2:
                 tmp2 = temp_dict
             else:
                 tmp2.update(temp_dict)
         stats[str(year)] = tmp2
         tmp2 = {}
-    
-    #Check if output is good 
+
+    #Check if output is good
     print(stats['2014']['Alex deQuevedo'])
     print(stats['2018']['Alex deQuevedo'])
     print(stats['2019']['Alex deQuevedo'])
-    
-    
-    
-    
+
+
+
+
     if print_json:
         with open('data.json', 'w') as outfile:
             json.dump(stats, outfile)
-            
-            
+
+
 # choose what to run
 def main():
-    
+    yearly_historical_inserts()
 
-    
+
 if __name__ == '__main__':
     main()
